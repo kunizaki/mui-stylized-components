643f8f0a44eb6d2060d25193807d04a9aef9d925
André Kunizaki <andre.kunizaki@rb.com.br>
2023-03-21 15:26:53 -0300

    teste

---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+import { Header } from './Header';
+import { Loading } from './Loading';
+import { Sidebar } from './Sidebar';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { Header, Loading, Sidebar}
